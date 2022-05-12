a697de2c48b81237c86ff8a73e96df5247c5a3e0
jefferson magallanes <jeffersonmaga97@gmail.com>
2022-05-11 21:36:53 -0300

    diseno responsive

--- complemet.js
+++ complemet.js
@@ -1,3 +1,6 @@
+let almacenados = [];
+let descuento = 1500;
+
 class celular {
     constructor(marca, precio, img, id, cantidad) {
         this.marca = marca;
@@ -5,24 +8,50 @@ class celular {
         this.img = img
         this.id = id;
         this.cantidad = cantidad;
+
     }
-
+    o
 }
-
 const galaxys22 = (new celular("samsung galaxy s22", "120000", "img/galaxys22.jpg", "01", "1"));
 const galaxya71 = (new celular("samsung Galaxy A71", "80000", "img/galaxyZflip.jpg ", "02", "1"));
 const galaxflip = (new celular("samsung galaxy z flipp", "160000", "img/samsunga71.jpg", "03", "1"));
-const motorolag71 = (new celular("Motorla Moto G71", "90000", "img/motoG71.jpg", "04", "1"));
-const motorolag60 = (new celular("Motorla Moto G60", "70000", "img/motorolaG60.jpg ", "05", "1"));
-const motorolaEdge = (new celular("Motorla Moto Edge", "120000", "img/motoEdge20.jpg", "06", "1"));
+const motorolag71 = (new celular("motorola Moto G71", "90000", "img/motoG71.jpg", "04", "1"));
+const motorolag60 = (new celular("motorola Moto G60", "70000", "img/motorolaG60.jpg ", "05", "1"));
+const motorolaEdge = (new celular("motorola Moto Edge", "120000", "img/motoEdge20.jpg", "06", "1"));
 const iphone13 = (new celular("Iphone 12 ", "150000", "img/iphone12.jpg ", "07", "1"));
 const iphone12 = (new celular("Iphone 13 ", "130000", "img/iphone13.jpg", "08", "1"));
 const iphonexr = (new celular("Iphone xr ", "110000", "img/iphonexr.jpg", "09", "1"));
-const carritoCelulares = [galaxys22, galaxya71, galaxflip, motorolag71, motorolag60, motorolaEdge, iphone12, iphone13, iphonexr];
+const xiaomiredmi = (new celular("Xiaomi Redmi note 11 ", "90000", "img/redmi11.jpg", "10", "1"));
+const xiaomi10 = (new celular("Xiaomi Redmi 10 Pro", "80000", "img/redmi10.jpg", "11", "1"));
+const xiaomi11 = (new celular("Xiaomi Redmi  9T Dual ", "75000", "img/redmi9.jpg", "12", "1"));
+const carritoCelulares = [galaxys22, galaxya71, galaxflip, motorolag71, motorolag60, motorolaEdge, iphone12, iphone13, iphonexr, xiaomiredmi, xiaomi10, xiaomi11];
 
-let almacenados = [];
-let descuento = 1500;
-//// Funcion que crea las card
+/*async function pedirRecurso() {
+        const respuesta = await fetch("http://localhost:5500/inforSimuladorServer.json")
+        const dataServer = await respuesta.json()
+        carritoCelulares = dataServer;
+        carritoCelulares.forEach((celular) => {
+            crearCards(celular);
+        }); 
+}
+
+pedirRecurso()*/
+
+
+/*fetch("/inforSimuladorServer.json")
+    .then((respuesta) => respuesta.json())
+    .catch((error) => console.log(error))
+    .then((infoServer) => {
+        carritoCelulares = infoServer;
+        carritoCelulares.forEach((celular) => {
+            crearCards(celular);
+        });
+        AsideSearch()
+    });
+*/
+
+
+//// Funcion que crea las card en DOM
 function crearCards(celular) {
     const rowdiv = document.getElementById("rowcard");
     let cards = document.createElement("div");
@@ -62,8 +91,9 @@ function AgregarCarrito(e) {
     almacenados.push(productoAgregado);
     const enJson = JSON.stringify(almacenados);
     localStorage.setItem("Productos", enJson);
-    const objparse = JSON.parse(localStorage.getItem("Productos"));
+    objparse = JSON.parse(localStorage.getItem("Productos"));
     actualizarTabla()
+
 }
 ///// habilito los botos y actualizo el local storage
 function actualizarBotonesyLocalStorage() {
@@ -124,7 +154,6 @@ function agregarDatos(celular) {
     aux.append(aumentarCantidad);
     aux.append(disminuirCantidad);
 
-
     /// splice y  boton eliminar
     let eliminarBtn = document.querySelector(".eliminar-item")
     eliminarBtn.className = "btn ";
@@ -133,7 +162,7 @@ function agregarDatos(celular) {
         almacenados.splice(locationItem, 1);
         actualizarTabla();
         actualizarBotonesyLocalStorage();
-        /////// ALERT DE LIBRERIA JS
+        /////// ALERT DE LIBRERIA JS SWEETalert2
         Swal.fire({
             title: 'Estas seguro ?',
             text: "Perderas los descuentos promocionales!",
@@ -159,6 +188,10 @@ function agregarDatos(celular) {
 
 //// actualizo la tabla 
 function actualizarTabla() {
+
+
+
+
     tabla.innerHTML = "";
     almacenados.forEach((item) => {
         agregarDatos(item);
@@ -178,6 +211,7 @@ function comprarBtn() {
     btnCompra.onclick = (e) => {
         e.target;
         e.preventDefault
+            /////// ALERT DE LIBRERIA JS SWEETalert2
         Swal.fire({
             position: 'top-end',
             icon: 'success',
@@ -185,18 +219,23 @@ function comprarBtn() {
             showConfirmButton: false,
             timer: 1500
         });
+        totalProductos.innerHTML = "";
+        totalPrecio.innerHTML = "";
+        tabla.innerHTML = "";
+        almacenados = [];
+        localStorage.setItem("Productos", JSON.stringify(almacenados));
+
     }
 }
-///// El evento del boton comprar solo se activa si hay elementos en el array
-if (almacenados.length !== 0) {
+
+///// Evento de boton comprar solo se activa si hay elementos AGREGADOS en el array
+if (almacenados.length >= 1) {
     comprarBtn();
     console.log("El carrito no está vacío Puedes comprar!");
 
 }
 
-
-
-//// side bar
+//// Elementos del side bar
 const openBtn = document.querySelector(".carri");
 const sideBarWrapper = document.querySelector("div.side-bar-wrapper");
 
@@ -220,4 +259,52 @@ document.querySelector("#side-bar").addEventListener("click", function(e) {
     e.stopPropagation();
 });
 
+//// Funcion que buscar telefonos a traves de un Input en barra de NAVegacion
+let btnNavSearch = document.querySelector(".botonBusqueda");
+btnNavSearch.addEventListener("click", function(e) {
+    e.preventDefault();
+    const rowdiv = document.getElementById("rowcard");
+    const inputNavSearch = document.querySelector(".barraBusqueda").value;
+    let encontrados = carritoCelulares.filter((celular) => celular.marca.toLowerCase().includes(inputNavSearch));
+    rowdiv.innerText = "";
+    for (const articulo of encontrados) {
+        crearCards(articulo);
+    }
+
+});
+
+//// Funcion que busca por marca a traves de UL en el Dom Y Oculta el parallax
+function AsideSearch() {
+    let divSearh = document.querySelector(".divBusqueda");
+    let ulSearch = document.createElement("ul");
+    ulSearch.className = ("navbar-nav");
+    ulSearch.innerHTML = `
+<p><b>Buscar por Marcas</b></p>
+<li><button id="iphone" class="btn searchMarca" value="iphone">Iphone</button></li>
+<li><button id="samsung" class="btn searchMarca" value="samsung">Samsung</button></li>
+<li><button id="motorola" class="btn searchMarca" value="motorola">Motorola</button></li>
+<li><button id="xiaomi" class="btn searchMarca" value="xiaomi">Xiaomi</button></li>
+<li><a href="index.html" class="btn">Mostrar <b>Todos</b></a></li>
+`
+    divSearh.append(ulSearch);
+
+    let btn = document.querySelectorAll(".searchMarca");
+    for (const boton of btn) {
+        boton.addEventListener("click", function(e) {
+            e.preventDefault();
+            let busquedaMarca = carritoCelulares.filter((celular) => celular.marca.toLowerCase().includes(e.target.id));
+            const rowdiv = document.getElementById("rowcard");
+            rowdiv.innerText = "";
+            ///Oculto el parallax
+            let ocultarParallax = document.getElementById("teAyudamos")
+            ocultarParallax.style.display = 'none';
+            for (const articulo of busquedaMarca) {
+                crearCards(articulo);
+            }
+        });
+
+    }
+}
+
+AsideSearch()
 actualizarTabla();
